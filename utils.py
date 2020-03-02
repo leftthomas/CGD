bb5118265a540907734e5c3f725e2150e67da0da
leftthomas <leftthomas@qq.com>
2020-03-02 13:10:38 +0800

    fix bug of BatchHardTripletLoss when data contains no (anchor positive negative) triplet

--- utils.py
+++ utils.py
@@ -99,15 +99,19 @@ class BatchHardTripletLoss(nn.Module):
     def forward(self, x, target):
         pairwise_dist = torch.cdist(x.unsqueeze(0), x.unsqueeze(0)).squeeze(0)
 
-        mask_anchor_positive = self.get_anchor_positive_triplet_mask(target).float()
-        anchor_positive_dist = mask_anchor_positive * pairwise_dist
+        mask_anchor_positive = self.get_anchor_positive_triplet_mask(target)
+        contains_anchor_positive = mask_anchor_positive.any(dim=-1, keepdim=True)
+        anchor_positive_dist = mask_anchor_positive.float() * pairwise_dist
         hardest_positive_dist = anchor_positive_dist.max(1, True)[0]
 
-        mask_anchor_negative = self.get_anchor_negative_triplet_mask(target).float()
+        mask_anchor_negative = self.get_anchor_negative_triplet_mask(target)
+        contains_anchor_negative = mask_anchor_negative.any(dim=-1, keepdim=True)
         max_anchor_negative_dist = pairwise_dist.max(1, True)[0]
-        anchor_negative_dist = pairwise_dist + max_anchor_negative_dist * (1.0 - mask_anchor_negative)
+        anchor_negative_dist = pairwise_dist + max_anchor_negative_dist * (1.0 - mask_anchor_negative.float())
         hardest_negative_dist = anchor_negative_dist.min(1, True)[0]
 
-        triplet_loss = F.relu(hardest_positive_dist - hardest_negative_dist + self.margin)
+        loss = (F.relu(hardest_positive_dist - hardest_negative_dist + self.margin))
+        contains_anchor_positive_negative = contains_anchor_positive & contains_anchor_negative
+        refined_loss = loss * contains_anchor_positive_negative.float()
 
-        return triplet_loss.sum()
+        return refined_loss.sum() / contains_anchor_positive_negative.float().sum()
