3ae3f0084c7a34fdb966102165fa99f94075d99d
leftthomas <leftthomas@qq.com>
2020-03-04 23:52:10 +0800

    fix bug about saved image size

--- test.py
+++ test.py
@@ -15,45 +15,41 @@ if __name__ == '__main__':
 
     opt = parser.parse_args()
 
-    QUERY_IMG_NAME, DATA_BASE, RETRIEVAL_NUM = opt.query_img_name, opt.data_base, opt.retrieval_num
-    DATA_NAME = DATA_BASE.split('_')[0]
+    query_img_name, data_base_name, retrieval_num = opt.query_img_name, opt.data_base, opt.retrieval_num
+    data_name = data_base_name.split('_')[0]
 
-    data_base = torch.load('results/{}'.format(DATA_BASE))
-    if QUERY_IMG_NAME not in data_base['{}_images'.format(DATA_TYPE)]:
-        raise FileNotFoundError('{} not found'.format(QUERY_IMG_NAME))
-    query_index = data_base['{}_images'.format(DATA_TYPE)].index(QUERY_IMG_NAME)
-    query_image = Image.open(QUERY_IMG_NAME).convert('RGB').resize((256, 256), resample=Image.BILINEAR)
-    query_label = data_base['{}_labels'.format(DATA_TYPE)][query_index]
-    query_feature = data_base['{}_features'.format(DATA_TYPE)][query_index]
+    data_base = torch.load('results/{}'.format(data_base_name))
 
-    gallery_images = data_base['{}_images'.format('train' if DATA_TYPE == 'train' else 'gallery')]
-    gallery_labels = data_base['{}_labels'.format('train' if DATA_TYPE == 'train' else 'gallery')]
-    gallery_features = data_base['{}_features'.format('train' if DATA_TYPE == 'train' else 'gallery')]
+    if query_img_name not in data_base['test_images']:
+        raise FileNotFoundError('{} not found'.format(query_img_name))
+    query_index = data_base['test_images'].index(query_img_name)
+    query_image = Image.open(query_img_name).convert('RGB').resize((224, 224), resample=Image.BILINEAR)
+    query_label = torch.tensor(data_base['test_labels'][query_index])
+    query_feature = data_base['test_features'][query_index]
 
-    query_label = torch.tensor(query_label)
-    query_feature = query_feature.view(1, *query_feature.size()).permute(1, 0, 2).contiguous()
-    gallery_labels = torch.tensor(gallery_labels)
-    gallery_features = gallery_features.permute(1, 2, 0).contiguous()
+    gallery_images = data_base['{}_images'.format('test' if data_name != 'isc' else 'gallery')]
+    gallery_labels = torch.tensor(data_base['{}_labels'.format('test' if data_name != 'isc' else 'gallery')])
+    gallery_features = data_base['{}_features'.format('test' if data_name != 'isc' else 'gallery')]
 
-    sim_matrix = query_feature.bmm(gallery_features).mean(dim=0).squeeze(dim=0)
-    if (DATA_NAME is not 'isc') or (DATA_NAME is 'isc' and DATA_TYPE is 'train'):
-        sim_matrix[query_index] = -1
-    idx = sim_matrix.argsort(dim=-1, descending=True)
+    dist_matrix = torch.cdist(query_feature.unsqueeze(0).unsqueeze(0), gallery_features.unsqueeze(0)).squeeze()
+    if data_name != 'isc':
+        dist_matrix[query_index] = float('inf')
+    idx = dist_matrix.argsort(dim=-1, descending=False)
 
-    result_path = 'results/{}'.format(QUERY_IMG_NAME.split('/')[-1].split('.')[0])
+    result_path = 'results/{}'.format(query_img_name.split('/')[-1].split('.')[0])
     if os.path.exists(result_path):
         shutil.rmtree(result_path)
     os.mkdir(result_path)
     query_image.save('{}/query_img.jpg'.format(result_path))
-    for num, index in enumerate(idx[:RETRIEVAL_NUM]):
+    for num, index in enumerate(idx[:retrieval_num]):
         retrieval_image = Image.open(gallery_images[index.item()]).convert('RGB') \
-            .resize((256, 256), resample=Image.BILINEAR)
+            .resize((224, 224), resample=Image.BILINEAR)
         draw = ImageDraw.Draw(retrieval_image)
         retrieval_label = gallery_labels[index.item()]
         retrieval_status = (retrieval_label == query_label).item()
-        retrieval_prob = sim_matrix[index.item()].item()
+        retrieval_dist = dist_matrix[index.item()].item()
         if retrieval_status:
-            draw.rectangle((0, 0, 255, 255), outline='green', width=8)
+            draw.rectangle((0, 0, 223, 223), outline='green', width=8)
         else:
-            draw.rectangle((0, 0, 255, 255), outline='red', width=8)
-        retrieval_image.save('{}/retrieval_img_{}_{}.jpg'.format(result_path, num + 1, '%.4f' % retrieval_prob))
+            draw.rectangle((0, 0, 223, 223), outline='red', width=8)
+        retrieval_image.save('{}/retrieval_img_{}_{}.jpg'.format(result_path, num + 1, '%.4f' % retrieval_dist))
